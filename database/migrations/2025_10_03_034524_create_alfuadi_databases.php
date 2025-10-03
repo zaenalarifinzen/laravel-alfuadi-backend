4fb3461966a26ba170c194c59f38fa12796b1ea8
Zaenal Arifin <zaenal.ariefin24@gmail.com>
2025-10-03 15:06:14 +0700

    add alfuadi databases migration

---
+++ database/migrations/2025_10_03_034524_create_alfuadi_databases.php
@@ -0,0 +1,70 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        // Surahs Table
+        Schema::create('surahs', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->string('name_id', 100);
+            $table->string('name_en', 100);
+            $table->string('location', 100);
+            $table->integer('verse_count');
+        });
+
+        // Verses Table
+        Schema::create('verses', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('surah_id')->constrained('surahs')->onDelete('cascade');
+            $table->text('text');
+        });
+
+        // Word Groups Table
+        Schema::create('word_groups', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('surah_number')->constrained('surahs')->onDelete('cascade');
+            $table->foreignId('verse_number')->constrained('verses')->onDelete('cascade');
+            $table->integer('order_number')->nullable();
+            $table->text('text');
+        });
+
+        // Words Table
+        Schema::create('words', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('word_group_id')->constrained('word_groups')->onDelete('cascade');
+            $table->integer('order_number');
+            $table->text('word');
+            $table->text('translation')->nullable();
+            $table->string('kalimat', 100)->nullable();
+            $table->string('jenis', 100)->nullable();
+            $table->string('hukum', 100)->nullable();
+            $table->string('mabni_detail', 100)->nullable();
+            $table->string('kategori', 100)->nullable();
+            $table->string('kedudukan', 100)->nullable();
+            $table->string('yang_diikuti', 100)->nullable();
+            $table->string('irab', 100)->nullable();
+            $table->string('tanda', 100)->nullable();
+            $table->string('nampak', 100)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('words');
+        Schema::dropIfExists('word_groups');
+        Schema::dropIfExists('verses');
+        Schema::dropIfExists('surahs');
+    }
+};
