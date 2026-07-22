77a1f39b70dd5eb15570e365265b8df597c54f13
Zaenal Arifin <zaenal.ariefin24@gmail.com>
2026-07-22 08:59:27 +0700

    install sentry/browser

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,12 @@
+import * as Sentry from "@sentry/browser";
 import axios from 'axios';
+
+Sentry.init({
+  dsn: "https://83731e66daafc69e939b25ccd84627b3@o4511772406906880.ingest.us.sentry.io/4511772465627136",
+  environment: import.meta.env.MODE,
+  tracesSampleRate: 0.1,
+});
+
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
